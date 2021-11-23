4ffb6b587404fb16a6b9c16f5756777839d9ceae
amir <amirsmvb@yahoo.com>
2021-11-24 01:26:40 +0330

    serilizer updated

--- api/serializers.py
+++ api/serializers.py
@@ -4,6 +4,4 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
-		# fields = "__all__"
-		# exclude= ['is_superuser','is_staff','is_active','date_joined','user_permissions','groups']
 		fields = ['username', 'first_name','last_name','email']
