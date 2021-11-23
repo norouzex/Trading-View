04bf721e69fb8d6943925f7da6bdf6173d9a8ce3
amir <amirsmvb@yahoo.com>
2021-11-24 01:59:39 +0330

    New superuser added

--- api/serializers.py
+++ api/serializers.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
-		fields = ['username', 'first_name','last_name','email']
+		fields = ['username', 'first_name', 'last_name', 'email']
