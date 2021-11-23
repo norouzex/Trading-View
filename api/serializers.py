37b9af27b3dc93b25856110812de0a884f7a1620
norouzy <mohamadnewday@gmail.com>
2021-11-24 00:11:30 +0330

    change user serializer

--- api/serializers.py
+++ api/serializers.py
@@ -5,4 +5,5 @@ class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
 		# fields = "__all__"
-		exclude= ['is_superuser','is_staff','is_active','date_joined','user_permissions','groups']
+		# exclude= ['is_superuser','is_staff','is_active','date_joined','user_permissions','groups']
+		fields = ['username', 'first_name','last_name','email']
