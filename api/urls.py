2af5735a34e4f72a56cb87e9ff88820846b80068
amir <amirsmvb@yahoo.com>
2021-12-11 01:53:27 +0330

    model paper_trading change to OneToOne with User user positions and user create positions created

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path, include
-from .views import UserList, UserDetail
+from django.urls import path
+from .views import *
+
 app_name = "api"
+
 
 urlpatterns = [
 	path('user/', UserList.as_view(),name="UserList"),
 	path('user/<int:pk>', UserDetail.as_view(), name="UserDetail"),
+
+	path('user/positions/', PositionList.as_view()),
+	path('user/positions/<int:pk>/', PositionDetail.as_view()),
+	path('user/positions/create/', PositionCreate.as_view()),
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,16 +1,52 @@
 from django.shortcuts import render
-from .serializers import UserSerializer
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, RetrieveAPIView
+from django.http import HttpResponse
+from rest_framework.views import APIView
+
+from .serializers import *
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, RetrieveAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
+from rest_framework import viewsets
 from django.contrib.auth.models import User
 from .permissions import IsSuperUser, IsUser
-# Create your views here.
+from .models import Position, Paper_trading
+
+from rest_framework.response import Response
+
 
 class UserList(ListCreateAPIView):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
 	permission_classes = (IsSuperUser,)
 
+
 class UserDetail(RetrieveUpdateAPIView):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
 	permission_classes = (IsUser,)
+
+
+class PositionList(ListCreateAPIView):
+	serializer_class = PositionSerializer
+	def get_queryset(self):
+		user = self.request.user
+		query = Position.objects.filter(paper_trading__user=user)
+		return query
+
+
+class PositionDetail(RetrieveUpdateDestroyAPIView):
+	serializer_class = PositionSerializer
+	def get_queryset(self):
+		user = self.request.user
+		query = Position.objects.filter(paper_trading__user=user)
+		return query
+
+class PositionCreate(CreateAPIView):
+	serializer_class = PositionAddSerializer
+	def get_queryset(self):
+		user = self.request.user
+		query = Position.objects.filter(paper_trading__user=user)
+		return query
+
+	def perform_create(self, serializer):
+		user = self.request.user
+		paper = user.paper_trading
+		serializer.save(paper_trading=paper)
