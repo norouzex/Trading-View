eba0195d9264cc0f53561d911ecedbf492ff0595
norouzy <mohamadnewday@gmail.com>
2021-12-11 23:34:36 +0330

    position option model timezoon

--- api/models.py
+++ api/models.py
@@ -72,7 +72,7 @@ class Position_option(models.Model):
     trade_type = models.CharField(max_length=1, choices=TRADE_TYPE_CHOISES, verbose_name="trade type", default="w")
     stoploss = models.FloatField(blank=True, null=True)
     take_profit = models.FloatField(blank=True, null=True)
-    oreder_reach_date = models.DateTimeField(default=timezone.now, null=True, blank=True)
+    oreder_reach_date = models.DateTimeField(null=True, blank=True)
 
 
     def position_name(self):
