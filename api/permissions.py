4d73927b6511c1cfae560eba567e96280efedb61
amir <amirsmvb@yahoo.com>
2021-12-11 23:23:51 +0330

    permissions for papertrading generated

--- api/permissions.py
+++ api/permissions.py
@@ -30,6 +30,14 @@ class UserPosition(BasePermission):
 				request.user.id == obj.paper_trading.user.id
 			)
 
+class UserPapertrading(BasePermission):
+	def has_object_permission(self, request, view, obj):
+		return bool(
+				request.user.is_authenticated and
+				request.user.is_superuser or
+				request.user.is_authenticated and
+				request.user.id == obj.user.id
+			)
 
 class IsStaffOrReadOnly(BasePermission):
 	def has_permission(self, request, view):
