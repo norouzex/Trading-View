8de7584d98b18b5af37c14b3979dda5e94e142fd
norouzy <mohamadnewday@gmail.com>
2021-12-12 18:26:37 +0330

    not important

--- api/views.py
+++ api/views.py
@@ -1,142 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.db import IntegrityError
-from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
+from django.urls import path, include
+from .views import *
+from rest_framework import routers
 
-from .serializers import *
+app_name = "api"
 
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, RetrieveAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView, ListAPIView, UpdateAPIView
-from rest_framework import viewsets
-from rest_framework import serializers
-from rest_framework.response import Response
+router = routers.SimpleRouter()
+router.register('paper-tradingg', PapertradingViewSet, basename='paper_trading')
 
-from .permissions import *
+urlpatterns = [
+	path('user/', UserList.as_view(),name="UserList"),
+	path('user/<int:pk>', UserDetail.as_view(), name="UserDetail"),
 
-from .models import Position
+	path('user/positions/', PositionList.as_view()),
+	path('user/positions/create/', PositionCreate.as_view()),
+	path('user/positions/<int:pk>/', PositionStatusUpdate.as_view()),
+	path('user/positions/<int:pk>/option/create/', PositionOption.as_view()),
+	path('user/positions/<int:in_position>/option/update/', PositionOptionDetail.as_view()),
 
+	path('positions/', PositionTotal.as_view()),
+	path('paper-trading/', PapertradingListView.as_view()),
+	path('paper-trading/<int:pk>/', PapertradingDetail.as_view()),
 
-User = get_user_model()
-
-
-
-class UserList(ListCreateAPIView):
-	queryset = User.objects.all()
-	serializer_class = UserSerializer
-	permission_classes = (IsSuperUser,)
-
-
-class UserDetail(RetrieveUpdateAPIView):
-	queryset = User.objects.all()
-	serializer_class = UserSerializer
-	permission_classes = (IsUser,)
-
-
-class PositionList(ListAPIView):
-	serializer_class = PositionSerializer
-	permission_classes = (IsUser,)
-	def get_queryset(self):
-		user = self.request.user
-		query = Position.objects.filter(paper_trading__user=user)
-		return query
-
-
-class PositionStatusUpdate(RetrieveUpdateDestroyAPIView):
-	serializer_class = PositionUpdateSerializer
-	permission_classes = (UserPosition,)
-	def get_queryset(self):
-		user = self.request.user
-		query = Position.objects.filter(paper_trading__user=user)
-		return query
-
-
-class PositionCreate(CreateAPIView):
-	serializer_class = PositionAddSerializer
-	def perform_create(self, serializer):
-		user = self.request.user
-		paper = user.paper_trading
-		serializer.save(paper_trading=paper)
-
-
-class PositionTotal(ListAPIView):
-	queryset = Position.objects.all()
-	serializer_class = PositionSerializer
-	permission_classes = (IsSuperUser,)
-
-
-class PositionOption(ListCreateAPIView):
-  # queryset = Position_option.objects.all()
-	serializer_class = PositionOptionSerializer
-	permission_classes = (UserPositionOption,)
-
-	def get_queryset(self):
-		user = self.request.user
-		id = self.kwargs['pk']
-		query = Position_option.objects.filter(in_position=id, in_position__paper_trading__user=user)
-		return query
-
-	def perform_create(self, serializer):
-		user = self.request.user
-		id = self.kwargs['pk']
-		position = Position.objects.get(id=id)
-		option = Position_option.objects.filter(in_position=position, in_position__paper_trading__user=user)
-		if not option:
-			serializer.save(in_position=position)
-		else:
-			raise serializers.ValidationError("You already have a position option")
-
-class PositionOptionViewset(viewsets.ModelViewSet):
-	serializer_class = PositionOptionSerializer
-	permission_classes = (UserPositionOption,)
-	lookup_field = "in_position"
-	def get_queryset(self):
-		user = self.request.user
-		query = Position_option.objects.filter(in_position__paper_trading__user=user)
-		return query
-
-	def perform_create(self, serializer):
-		user = self.request.user
-		serializer.save(in_position__paper_trading__user=user)
-
-class PapertradingViewSet(viewsets.ModelViewSet):
-	serializer_class = PaperTradingSerializer
-	permission_classes = (UserPapertrading,)
-
-	def get_queryset(self):
-		user = self.request.user
-		query = Paper_trading.objects.filter(user=user)
-		return query
-
-	def perform_create(self, serializer):
-		user = self.request.user
-		serializer.save(user=user)
-
-
-class PapertradingListView(ListCreateAPIView):
-	serializer_class = PaperTradingSerializer
-	permission_classes = (IsUser,)
-
-	def get_queryset(self):
-		user = self.request.user
-		query = Paper_trading.objects.filter(user=user)
-		return query
-
-	def perform_create(self, serializer):
-		user = self.request.user
-
-		try:
-			serializer.save(user=user)
-		except IntegrityError:
-			raise serializers.ValidationError("You already have a paper account")
-		else:
-			raise serializers.ValidationError("Something went wrong")
-
-
-class PapertradingDetail(RetrieveUpdateDestroyAPIView):
-	serializer_class = PaperTradingSerializer
-	permission_classes = (UserPapertrading,)
-
-	def get_queryset(self):
-		user = self.request.user
-		query = Paper_trading.objects.filter(user=user)
-		return query
+	path('', include(router.urls))
+]
