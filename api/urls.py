71848ccc4aacc483dd627379dea841cf1d4e5d21
amir <amirsmvb@yahoo.com>
2021-12-12 18:15:50 +0330

    an url generated for PositionOptionDetail View

--- api/urls.py
+++ api/urls.py
@@ -6,16 +6,16 @@ app_name = "api"
 
 router = routers.SimpleRouter()
 router.register('paper-tradingg', PapertradingViewSet, basename='paper_trading')
-router.register('user/position-option', PositionOptionViewset, basename='position-option')
 
 urlpatterns = [
 	path('user/', UserList.as_view(),name="UserList"),
 	path('user/<int:pk>', UserDetail.as_view(), name="UserDetail"),
 
 	path('user/positions/', PositionList.as_view()),
-	path('user/positions/<int:pk>/', PositionStatusUpdate.as_view()),
-	path('user/positions/<int:pk>/option/', PositionOption.as_view()),
 	path('user/positions/create/', PositionCreate.as_view()),
+	path('user/positions/<int:pk>/', PositionStatusUpdate.as_view()),
+	path('user/positions/<int:pk>/option/create/', PositionOption.as_view()),
+	path('user/positions/<int:in_position>/option/update/', PositionOptionDetail.as_view()),
 
 	path('positions/', PositionTotal.as_view()),
 	path('paper-trading/', PapertradingListView.as_view()),
@@ -23,3 +23,4 @@ urlpatterns = [
 
 	path('', include(router.urls))
 ]
+
