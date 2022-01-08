8805477859d2d636905d7f904d9fa8f0d0242862
amir <amirsmvb@yahoo.com>
2022-01-09 02:17:40 +0330

    webscket sets

--- config/asgi.py
+++ config/asgi.py
@@ -8,10 +8,21 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.conf.urls import url
 from django.core.asgi import get_asgi_application
+from .routing import ws_pattern
+from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
 
+    # WebSocket chat handler
+    "websocket": JWTAuthMiddlewareStack(URLRouter(ws_pattern))
+})
