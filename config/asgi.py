cb79e0e655678e36d976af83a58881914eb75539
amir <amirsmvb@yahoo.com>
2022-01-10 12:40:47 +0330

    updated

--- config/asgi.py
+++ config/asgi.py
@@ -53,7 +53,6 @@ from django.core.asgi import get_asgi_application
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
