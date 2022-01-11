05d707bb336cf740d34dc694f2eae94c2ca31f48
Mohamad Noroozi <77892796+norouzy@users.noreply.github.com>
2022-01-11 02:16:58 +0100

    Update routing.py

--- config/routing.py
+++ config/routing.py
@@ -3,7 +3,7 @@ from channels.auth import AuthMiddlewareStack
 from django.urls import path
 from api import consumer
 # from .middleware import TokenAuthMiddleware
-from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
+# from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
 
 ws_pattern= [
     path('ws/user_info/',consumer.TradeConsumer.as_asgi()),
