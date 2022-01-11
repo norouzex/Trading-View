e7dfc462941108dde064e30d88c67fe8aedcf471
Mohamad Noroozi <77892796+norouzy@users.noreply.github.com>
2022-01-11 02:30:16 +0100

    Update routing.py

--- config/routing.py
+++ config/routing.py
@@ -1,17 +1,33 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+# from django.urls import path
+# from api import consumer
+# # from .middleware import TokenAuthMiddleware
+# # from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
+
+# ws_pattern= [
+#     path('ws/user_info/',consumer.TradeConsumer.as_asgi()),
+#     path('ws/homeData/', consumer.HomePageConsumer.as_asgi()),
+# ]
+
+# # application = ProtocolTypeRouter(
+# #     {
+# #         'websocket': JWTAuthMiddlewareStack(URLRouter(ws_pattern))
+# #     }
+# # )
+from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
 from api import consumer
 # from .middleware import TokenAuthMiddleware
-# from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
+from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
 
 ws_pattern= [
     path('ws/user_info/',consumer.TradeConsumer.as_asgi()),
-    path('ws/homeData/', consumer.HomePageConsumer.as_asgi()),
 ]
 
-# application = ProtocolTypeRouter(
-#     {
-#         'websocket': JWTAuthMiddlewareStack(URLRouter(ws_pattern))
-#     }
-# )
+application= ProtocolTypeRouter(
+    {
+        'websocket':JWTAuthMiddlewareStack(URLRouter(ws_pattern))
+    }
+)
