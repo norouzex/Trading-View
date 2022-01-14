193d88e69966b162356b89e085199d755c6374b2
norouzy <mohamadnewday@gmail.com>
2022-01-14 14:02:26 +0330

    add api url

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,7 @@ from dj_rest_auth.views import PasswordResetConfirmView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
+    path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api/rest-auth/', include('dj_rest_auth.urls')),
     path('api/rest-auth/registration/', include('dj_rest_auth.registration.urls'))
